024dd1abe12236873a51bee6f0c5254c88019dfc
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-14 14:10:07 +0300

    Started Solving Leet Code Top Interview 150 Arrays and Strings

---
+++ TopInterview150/ArrayAndString/27RemoveElement/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _27RemoveElement;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [0, 1, 2, 2, 3, 0, 4, 2];
+        int val = 2;
+        Console.WriteLine(RemoveElement(nums, val));
+    }
+
+    public static int RemoveElement(int[] nums, int val)
+    {
+        //It moves when a non val element is found 
+        int pointer = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                if (pointer != i)
+                {
+                    nums[pointer] = nums[i];
+                }
+
+                pointer++;
+            }
+        }
+
+        return pointer;
+    }
+
+    public static int RemoveElementTwoPointersApproach(int[] nums, int val)
+    {
+        int left = 0;
+        int right = 0;
+
+        while (left <= right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left] = nums[right];
+                right--;
+            }
+
+            else
+            {
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ TopInterview150/ArrayAndString/MergeSortedArray/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var program = new Program();
+        int[] nums1 = [2, 0];
+        int nums1Length = 1;
+        int[] nums2 = [1];
+        int nums2Length = 1;
+        program.Merge(nums1, nums1Length, nums2, nums2Length);
+
+        foreach (int number in nums1)
+        {
+            System.Console.Write($"{number}");
+        }
+    }
+
+    public void Merge(int[] nums1,
+                      int nums1Length,
+                      int[] nums2,
+                      int nums2Length)
+    {
+        int pointer1 = nums1Length - 1;
+        int pointer2 = nums2Length - 1;
+        int sortedCount = (nums1Length + nums2Length) - 1;
+
+        while (pointer1 >= 0 && pointer2 >= 0)
+        {
+            if (nums1[pointer1] > nums2[pointer2])
+            {
+                nums1[sortedCount] = nums1[pointer1];
+                pointer1--;
+            }
+
+            else
+            {
+                nums1[sortedCount] = nums2[pointer2];
+                pointer2--;
+            }
+
+            sortedCount--;
+        }
+
+        //Copy remaining elements to nums1
+        while (pointer2 >= 0)
+        {
+            nums1[sortedCount] = nums2[pointer2];
+            sortedCount--;
+            pointer2--;
+        }
+    }
+}
