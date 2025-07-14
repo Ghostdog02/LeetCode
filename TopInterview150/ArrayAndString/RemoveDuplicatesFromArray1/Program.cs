b2f69a830ef8cd8c2306113adcc715c93d223b31
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-14 20:26:06 +0300

    Optimized RemovedDuplicatesMethod

---
+++ TopInterview150/ArrayAndString/RemoveDuplicatesFromArray1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RemoveDuplicatesFromArray1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [0, 0, 1, 1, 1, 2, 2];
+        Console.WriteLine(RemoveDuplicates1(nums));
+    }
+
+    public static int RemoveDuplicates1(int[] nums)
+    {
+        int pointer = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[pointer])
+            {
+                nums[++pointer] = nums[i];
+            }
+        }
+
+        return pointer + 1;
+    }
+}
