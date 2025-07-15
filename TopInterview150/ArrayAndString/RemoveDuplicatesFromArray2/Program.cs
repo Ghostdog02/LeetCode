cc4c989e447d4adf4078301a2d1054236bd262f2
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-15 14:31:56 +0300

    Solved Removed Duplicates From array 2 with O(N^2) time complexity

---
+++ TopInterview150/ArrayAndString/RemoveDuplicatesFromArray2/Program.cs
@@ -0,0 +1,77 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static void LinearTimeApproach()
+    {
+        
+    }
+
+    public static void QuadraticRuntimeApproach()
+    {
+        int[] nums = [0, 0, 1, 1, 1, 1, 2, 2, 2, 4];
+
+        int pointer = 0;
+        int end = nums.Length;
+
+        //If counter = 2 it means there are three ocurrences
+        int duplcateCounter = 0;
+
+        for (int i = 1; i < end; i++)
+        {
+            if (nums[pointer] == nums[i])
+            {
+                duplcateCounter++;
+            }
+
+            if (duplcateCounter == 2)
+            {
+                int lastPointerValue = nums[pointer];
+                int currentValue = 0;
+
+                do
+                {
+                    int tempIndex = i;
+                    lastPointerValue = nums[pointer];
+                    while (tempIndex < end - 1)
+                    {
+                        nums[tempIndex] = nums[tempIndex + 1];
+                        tempIndex++;
+                    }
+
+                    nums[tempIndex] = 0;
+
+                    if (end == i)
+                    {
+                        break;
+                    }
+
+                    end--;
+                    duplcateCounter--;
+
+                    pointer = i - 1;
+
+                    currentValue = nums[i];
+                }
+
+                while (lastPointerValue == currentValue);
+            }
+
+            if (nums[pointer] != nums[i])
+            {
+                pointer = i;
+                duplcateCounter = 0;
+            }
+        }
+
+        foreach (int num in nums)
+        {
+            Console.Write($"{num}, ");
+        }
+
+        Console.WriteLine($"UniquePairs: {end}");
+    }
+}
