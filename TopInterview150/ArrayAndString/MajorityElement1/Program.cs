8649b4eaf8407e3810fcf8f97f7a491aa3d612a9
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-17 19:00:36 +0300

    Solved Majority 1 Element Problem with Boyer-Moore Majority Voting Algorithm

---
+++ TopInterview150/ArrayAndString/MajorityElement1/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [2, 9, 0, 1, 1, 1, 1];
+        Console.WriteLine(FindMajorityElement(nums));
+    }
+
+    public static int FindMajorityElement(int[] nums)
+    {
+        int votes = 0;
+        int candidate = -1;
+
+        for (int index = 0; index < nums.Length; index++)
+        {
+            if (votes == 0)
+            {
+                candidate = nums[index];
+                votes = 1;
+            }
+
+            else if (nums[index] == candidate)
+            {
+                votes++;
+            }
+
+            else
+            {
+                votes--;
+            }
+        }
+
+        int count = 0;
+        for (int index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] == candidate)
+            {
+                count++;
+            }
+
+            if (count > nums.Length / 2)
+            {
+                return candidate;
+            }
+        }
+
+        return -1;
+    }
+}
