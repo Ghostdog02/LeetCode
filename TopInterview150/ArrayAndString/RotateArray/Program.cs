5c5b6eee7951ef1fa88bf3eefdd784e8db76ca3c
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-18 12:34:24 +0300

    Solved RotateArray Problem with O(N) time and space complexity solution

--- TopInterview150/ArrayAndString/RotateArray/Program.cs
+++ TopInterview150/ArrayAndString/RotateArray/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] nums = [-1, -100, 3, 99];
+int rotateAmount = 2;
+
+int length = nums.Length;
+bool[] visited = new bool[length];
+int lastrotatedIndex = 0;
+int lastReplacedNumber = 0;
+
+
+
+for (int counter = 0; counter < length; counter++)
+{
+    int rotation = lastrotatedIndex + rotateAmount;
+    int rotatedIndex = rotation >= length ? rotation % length : rotation;
+
+    while (visited[rotatedIndex])
+    {
+        lastrotatedIndex++;
+        rotation = lastrotatedIndex + rotateAmount;
+        rotatedIndex = rotation >= length ? rotation % length : rotation;
+        lastReplacedNumber = nums[lastrotatedIndex];
+    }
+
+    int temp = nums[rotatedIndex];
+
+    if (counter == 0)
+    {
+        nums[rotatedIndex] = nums[0];
+    }
+
+    else
+    {
+        nums[rotatedIndex] = lastReplacedNumber;
+    }
+
+    visited[rotatedIndex] = true;
+
+    lastrotatedIndex = rotatedIndex;
+    lastReplacedNumber = temp;
+
+}
+
+foreach (int number in nums)
+{
+    System.Console.Write($"{number}, ");
+}
