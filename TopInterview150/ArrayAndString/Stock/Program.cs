19ae5e2ac630cce76a19f4bd84ddb4f1e6e36af3
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-21 10:24:05 +0300

    Solved Stock Problem with O(N) time complexity

---
+++ TopInterview150/ArrayAndString/Stock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+int[] prices = [7, 1, 5, 3, 6, 4];
+
+int biggestProfit = 0;
+int cheapestPrice = prices[0];
+
+for (int i = 0; i < prices.Length; i++)
+{
+    if (prices[i] < cheapestPrice)
+    {
+        cheapestPrice = prices[i];
+    }
+
+    else if (i != 0)
+    {
+        int currProfit = prices[i] - cheapestPrice;
+        biggestProfit = biggestProfit < currProfit ? currProfit : biggestProfit;
+    }
+}
+
+System.Console.WriteLine(biggestProfit);
