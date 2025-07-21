d7986cb78993f1c728f35da322d1fc3c4cce7a42
Ghostdog02 <alex.vesely07@gmail.com>
2025-07-21 16:50:08 +0300

    SolvedStock2 and JumpGame1 problems

---
+++ TopInterview150/ArrayAndString/JumpGame1/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0];
+
+        
+        int currJump = nums[0];
+        bool[] visited = new bool[nums.Length];
+
+        int allowedTries = nums.Sum();
+
+        for (int i = 0; i < allowedTries; i++)
+        {
+            if (currJump >= nums.Length - 1)
+            {
+                System.Console.WriteLine(true);
+                return;
+            }
+
+            else if (nums[currJump] == 0 || visited[currJump])
+            {
+                currJump--;
+
+                if (currJump < 0)
+                {
+                    System.Console.WriteLine("false");
+                    return;
+                }
+            }
+
+            if (!visited[currJump])
+            {
+                if (currJump < visited.Length)
+                {
+                    visited[currJump] = true;
+                }
+
+                currJump += nums[currJump];
+            }
+
+
+
+        }
+
+        System.Console.WriteLine("false");
+    }
+}
---
+++ TopInterview150/ArrayAndString/Stock2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Stock2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] prices = [7, 6, 4, 3, 1];
+
+        int profit = 0;
+
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            if (prices[i] < prices[i + 1])
+            {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+
+        System.Console.WriteLine(profit);
+    }
+}
