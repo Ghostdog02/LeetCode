0df76dc83f18e64acd286f94330adb85bcd15d47
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-06 12:32:26 +0300

    Solved Find HINdex

---
+++ TopInterview150/ArrayAndString/HIndex/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = [10, 8, 5, 4, 3];
+
+        //SelectSort(numbers);
+        Console.WriteLine(FindHIndex(numbers));
+    }
+
+    public static int FindHIndex(int[] numbers)
+    {
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+
+        if (numbers.Length == 1)
+        {
+            return numbers[0] == 0 ? 0 : 1;
+        }
+
+        int hIndex = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > i)
+            {
+                hIndex++;
+            }
+
+            else
+            {
+                break;
+            }
+        }
+
+        return hIndex;
+    }
+}
