50af7569cf53c5de6e901ea737585f31ffa5533d
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-08 10:46:11 +0300

    Solved TwoSum problem

---
+++ TopInterview150/TwoPointers/167TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numbers = [-10, -5, -3, 0, 5, 8];
+int target = -8;
+
+int left = 0;
+int right = 1;
+int currSum;
+int end = numbers.Length - 1;
+
+while ((currSum = numbers[left] + numbers[right]) != target)
+{
+    if (currSum < target)
+    {
+        if (right < end)
+        {
+            right++;
+        }
+
+        else
+        {
+            left++;
+        }
+    }
+
+    else if (currSum > target)
+    {
+        right--;
+        end = right;
+        // left++;
+    }
+}
+
+System.Console.WriteLine($"Left: {left + 1}");
+System.Console.WriteLine($"Right: {right + 1}");
