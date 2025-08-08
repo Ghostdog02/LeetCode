eab87500ad3f33b08039b3512e00722ac2e99261
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-08 13:04:56 +0300

    Solved WaterContainer Problem with O(N) time and O(1) space complexity

---
+++ TopInterview150/TwoPointers/WaterContainer/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] heights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+        int left = 0;
+        int right = heights.Length - 1;
+
+        int maxArea = int.MinValue;
+        int currArea;
+
+        int height;
+        int length;
+
+        while (left < right)
+        {
+            height = Math.Min(heights[left], heights[right]);
+            length = Math.Abs(left - right);
+            currArea = height * length;
+
+            if (currArea > maxArea)
+            {
+                maxArea = currArea;
+            }
+
+            if (heights[left] < heights[right])
+            {
+                left++;
+            }
+
+            else
+            {
+                right--;
+            }
+        }
+
+        System.Console.WriteLine(maxArea);
+    }
+
+    public static int CalculateArea(int left, int right, int[] heights)
+    {
+        int height = Math.Min(heights[left], heights[right]);
+        int length = Math.Abs(left - right);
+
+        return height * length;
+    }
+}
