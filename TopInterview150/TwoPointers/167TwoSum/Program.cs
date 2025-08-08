6ead016e57cebb9a89f2f64052033bb4670ec415
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-08 10:55:46 +0300

    Optimized solution

--- TopInterview150/TwoPointers/167TwoSum/Program.cs
+++ TopInterview150/TwoPointers/167TwoSum/Program.cs
@@ -2,30 +2,19 @@
 int target = -8;
 
 int left = 0;
-int right = 1;
+int right = numbers.Length - 1;
 int currSum;
-int end = numbers.Length - 1;
 
 while ((currSum = numbers[left] + numbers[right]) != target)
 {
     if (currSum < target)
     {
-        if (right < end)
-        {
-            right++;
-        }
-
-        else
-        {
-            left++;
-        }
+        left++;
     }
 
     else if (currSum > target)
     {
         right--;
-        end = right;
-        // left++;
     }
 }
 
