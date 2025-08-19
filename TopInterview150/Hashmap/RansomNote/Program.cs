8e9d8ddbbe5193031b96f9b02d8e060ec28c127b
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-19 14:06:13 +0300

    Solved Problem with O(N + M) time and O(N) space complexity

---
+++ TopInterview150/Hashmap/RansomNote/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string ransomNote = "bg",
+            magazine = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+
+        Dictionary<char, int> bigDictionary = new();
+        char currLetter;
+        int occurrences;
+
+        for (int i = 0; i < magazine.Length; i++)
+        {
+            currLetter = magazine[i];
+            if (bigDictionary.TryGetValue(currLetter, out occurrences))
+            {
+                bigDictionary[currLetter]++;
+            }
+
+            else
+            {
+                bigDictionary.Add(currLetter, 1);
+            }
+        }
+
+        occurrences = 0;
+        
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            currLetter = ransomNote[i];
+            if (!bigDictionary.TryGetValue(currLetter, out occurrences) || occurrences <= 0)
+            {
+                System.Console.WriteLine(false);
+                return;
+            }
+
+            bigDictionary[currLetter]--;
+        }
+
+        System.Console.WriteLine(true);
+    }
+}
