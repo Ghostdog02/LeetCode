747c62c272c7aea6be79c4ddd3870a1576a40127
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-20 13:49:28 +0300

    Optimized approach by using array of char for storing the count of letters

--- TopInterview150/Hashmap/GroupAnagrams/Program.cs
+++ TopInterview150/Hashmap/GroupAnagrams/Program.cs
@@ -2,50 +2,29 @@
 
 string[] input = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
-// var anagramPairs = new Dictionary<string, IList<string>>();
-// var occurrences = new Dictionary<char, int>();
-// var sortedString = new StringBuilder();
-// int value;
-// IList<string> anagrams;
-// string currString;
+var anagramPairs = new Dictionary<string, IList<string>>();
 
-// for (int i = 0; i < input.Length; i++)
-// {
-//     currString = input[i];
-//     for (int currLetter = 0; currLetter < currString.Length; currLetter++)
-//     {
-//         if (occurrences.TryGetValue(currString[currLetter], out value))
-//         {
-//             occurrences[currString[currLetter]]++;
-//         }
+string sortedString;
+IList<string> anagrams;
 
-//         else
-//         {
-//             occurrences.Add(currString[currLetter], 1);
-//         }
-//     }
+foreach (string currString in input)
+{
+    char[] count = new char[26];
+    foreach (char letter in currString)
+    {
+        count[letter - 'a']++;
+    }
 
-//     for (char currLetter = 'a'; currLetter <= 'z'; currLetter++)
-//     {
-//         if (occurrences.TryGetValue(currLetter, out value))
-//         {
-//             sortedString.Append(currLetter, occurrences[currLetter]);
-//         }
-//     }
+    sortedString = new string(count);
 
-//     if (anagramPairs.TryGetValue(sortedString.ToString(), out anagrams))
-//     {
-//         anagrams.Add(currString);
-//     }
+    if (anagramPairs.TryGetValue(sortedString, out anagrams))
+    {
+        anagramPairs[sortedString].Add(currString);
+    }
+    else
+    {
+        anagramPairs.Add(sortedString, [currString]);
+    }
+}
 
-//     else
-//     {
-//         anagramPairs.Add(sortedString.ToString(), [currString]);
-//     }
-
-//     occurrences.Clear();
-//     sortedString.Clear();
-// }
-
-// var vallues = anagramPairs.Values.ToList();
-
+var vallues = anagramPairs.Values.ToList();
