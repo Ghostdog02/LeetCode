e02d4088aacd2090b46f387081b7a17a228058d4
Ghostdog02 <alex.vesely07@gmail.com>
2025-08-21 15:29:18 +0300

    Refactored code

--- TopInterview150/Hashmap/LongestConsecutiveSequence/Program.cs
+++ TopInterview150/Hashmap/LongestConsecutiveSequence/Program.cs
@@ -1,7 +1,5 @@
 ﻿public class Program
 {
-    
-
     public static void Main(string[] args)
     {
         var solution = new Solution();
@@ -13,98 +11,29 @@
 
 public class Solution
 {
-    public int CurrNumber { get; set; }
-
-    public int SequenceLength { get; set; }
-
-    public Dictionary<int, List<int>> Sequences { get; set; }
-
-    public int LongestSequenceLength { get; set; }
-
-    public Dictionary<int, bool> VisitedNumbers { get; set; }
-
-    public Solution()
-    {
-        SequenceLength = 1;
-        Sequences = new Dictionary<int, List<int>>();
-        VisitedNumbers = new Dictionary<int, bool>();
-    }
-
     public int CalculateLongestSequenceLength(int[] nums)
     {
-        foreach (int currNumber in nums)
+        var numSet = new HashSet<int>(nums);
+        int maxSequenceLength = 0;
+        int currSequenceLength = 1;
+        int currNumber;
+
+        foreach (int number in numSet)
         {
-            if (!Sequences.TryGetValue(currNumber, out _))
+            if (!numSet.Contains(number - 1))
             {
-                Sequences.Add(currNumber, new List<int>());
+                currNumber = number + 1;
+                while (numSet.Contains(currNumber))
+                {
+                    currSequenceLength++;
+                    currNumber++;
+                }
             }
+
+            maxSequenceLength = Math.Max(maxSequenceLength, currSequenceLength);
+            currSequenceLength = 1;
         }
 
-        foreach (var keyValuePair in Sequences)
-        {
-            int key = keyValuePair.Key;
-
-            if (!VisitedNumbers.TryGetValue(key, out _))
-            {
-                bool hasNext = Sequences.TryGetValue(key + 1, out _);
-                bool hasPrev = Sequences.TryGetValue(key - 1, out _);
-                if (hasNext && hasPrev)
-                {
-                    IterateSequence(key, false);
-                    IterateSequence(key, true);
-                }
-                else if (hasNext)
-                {
-                    IterateSequence(key, true);
-                }
-                else if (hasPrev)
-                {
-                    IterateSequence(key, false);
-                }
-                else
-                {
-                    VisitedNumbers.Add(key, true);
-                }
-
-                if (SequenceLength > LongestSequenceLength)
-                {
-                    LongestSequenceLength = SequenceLength;
-                }
-
-                CurrNumber = 0;
-                SequenceLength = 1;
-            }
-        }
-
-        return LongestSequenceLength;
-    }
-
-    public void IterateSequence(int key, bool isIncrementing)
-    {
-        switch (isIncrementing)
-        {
-            case true:
-                CurrNumber = key + 1;
-                break;
-            case false:
-                CurrNumber = key - 1;
-                break;
-        }
-
-        while (Sequences.TryGetValue(CurrNumber, out _))
-        {
-            VisitedNumbers.Add(CurrNumber, true);
-            Sequences[key].Add(CurrNumber);
-            SequenceLength++;
-            switch (isIncrementing)
-            {
-                case true:
-                    CurrNumber++;
-                    break;
-                case false:
-                    CurrNumber--;
-                    break;
-            }
-        }
+        return maxSequenceLength;
     }
 }
