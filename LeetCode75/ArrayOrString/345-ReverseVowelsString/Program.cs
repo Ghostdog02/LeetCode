b9a87227226413d4ec56bc81c851b0af6797ec5c
Ghostdog02 <alex.vesely07@gmail.com>
2026-05-04 13:40:25 +0300

    Solved 345 Reverse String Vowels

---
+++ LeetCode75/ArrayOrString/345-ReverseVowelsString/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace _345_ReverseVowelsString;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "leetcode";
+        Console.WriteLine(ReverseVowels(s));
+    }
+
+    static string ReverseVowels(string s) {
+        var vowels = new List<char>
+        {
+           'a', 'e', 'i', 'o', 'u',
+           'A', 'E', 'I', 'O', 'U'  
+        };
+        var builder = new StringBuilder();
+
+        int leftPointer = 0;
+        int rightPointer = s.Length - 1;
+
+        while (leftPointer < s.Length)
+        {
+            if (vowels.Contains(s[leftPointer]))
+            {
+                while (!vowels.Contains(s[rightPointer]))
+                {
+                    rightPointer--;
+                }
+
+                builder.Append(s[rightPointer]);
+                rightPointer--;
+            }
+
+            else
+            {
+                builder.Append(s[leftPointer]);
+            }
+
+            leftPointer++;
+        }
+
+        return builder.ToString();
+    }
+}
