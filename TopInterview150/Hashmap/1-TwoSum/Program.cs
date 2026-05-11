b4f9418c22e4de138e727d0d4908254c88ad70b2
Ghostdog02 <alex.vesely07@gmail.com>
2026-05-11 11:09:50 +0300

    Solved Queue, Binary Search Problems

---
+++ TopInterview150/Hashmap/1-TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1_TwoSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = TwoSum(new int[] { 3, 2, 4 }, 6);
+
+        System.Console.WriteLine( string.Join(", ", arr));
+    }
+
+    static int[] TwoSum(int[] nums, int target)
+    {
+        var set = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (set.Contains(target - nums[i]))
+            {
+                
+            }
+        }
+
+        return new int[] { };
+    }
+}
