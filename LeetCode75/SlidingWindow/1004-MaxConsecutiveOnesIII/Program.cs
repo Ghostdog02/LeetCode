d4962e364a4a6f107818c1b4f44fae647b3b8399
Ghostdog02 <alex.vesely07@gmail.com>
2026-05-12 18:38:16 +0300

    Solved one exercise from Sliding Window

---
+++ LeetCode75/SlidingWindow/1004-MaxConsecutiveOnesIII/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1004_MaxConsecutiveOnesIII;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
+        int k = 2;
+        Console.WriteLine(LongestOnes(nums, k));
+    }
+
+    static int LongestOnes(int[] nums, int k)
+    {
+
+        return 0;
+    }
+}
---
+++ LeetCode75/SlidingWindow/1456-MaximumVowelsInSubstring/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace _1456_MaximumVowelsInSubstring;
+
+class Program
+{
+    public int MaxVowelsCount = 0;
+
+    static void Main(string[] args)
+    {
+        string s = "weallloveyou";
+        int k = 7;
+        System.Console.WriteLine(MaxVowels(s, k));
+    }
+
+    static int MaxVowels(string s, int k) {
+        var vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+        int countVowels = 0;
+        int maxVowelsCount = 0;
+        int count = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < s.Length)
+        {
+            if (vowels.Contains(s[right]))
+            {
+                countVowels++;
+            }
+            
+            right++;
+            count++;
+
+            if (count == k)
+            {
+                maxVowelsCount = maxVowelsCount < countVowels ? countVowels : maxVowelsCount;
+                if (vowels.Contains(s[left]))
+                {
+                    countVowels--;
+                }
+                
+                left++;
+                count--;
+            }
+        }
+
+        return maxVowelsCount;
+    }
+}
